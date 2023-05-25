df9ec14f872caf97220564ba273e6c372cb8c755
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2023-05-25 19:59:53 +0500

    Initial Commit

---
+++ Cannabis/CannabisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CannabisApp.swift
+//  Cannabis
+//
+//  Created by Muhammad Farid Ullah on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CannabisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
